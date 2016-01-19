c5eb77c12f8fbd497d90103291be3ac01e1dbfca
Timothy \"Manu\" Meixell & Pratik Shah <pair+misternu.pratikmshah@devbootcamp.com>
2016-01-19 17:08:16 -0600

    Add articles to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 10.times do |x|
   Category.create(title: Faker::Lorem.word)
 end
+
+Category.all.each do |category|
+  10.times do
+    category.articles.create!(title: Faker::Lorem.word, body: Faker::Lorem.sentence)
+  end
+end
